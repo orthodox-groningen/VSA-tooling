b07402e26e7c8d40f4f1185e09d4fb7b566303d3
Rieks <RieksJ@users.noreply.github.com>
2026-06-26 06:49:59 +0200

    lokale link test script

--- scripts/test-links.cmd
+++ scripts/test-links.cmd
@@ -1,8 +1,10 @@
 @echo off
+REM Dit is een script om voor de lokale site te testen of links kloppen.
+REM volgens mij werkt dat nog niet helemaal...
 setlocal
 
 cd /d %~dp0\..
 
 call .venv\Scripts\activate
 
-python scripts\check-publication-output.py --site-dir generated\site --url-prefix /VSA-tooling/
+python scripts\check-publication-output.py --site-dir generated\site --url-prefix /
